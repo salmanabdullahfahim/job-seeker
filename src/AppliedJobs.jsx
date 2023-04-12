a4688df2599fe205dfbe8d7ce4410676751224e5
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-04-12 16:01:03 +0600

    applied job page problem solved

--- src/AppliedJobs.jsx
+++ src/AppliedJobs.jsx
@@ -3,44 +3,44 @@ import { useLoaderData } from 'react-router-dom';
 import ApplyJob from './ApplyJob';
 
 const AppliedJobs = () => {
-  const jobs = useLoaderData();
-  const savedJob = JSON.parse(localStorage.getItem('job-id'));
+    const jobs = useLoaderData();
 
-  const [filter, setFilter] = useState(null);
+    const savedJob = JSON.parse(localStorage.getItem('job-id'));
 
-  const jobCart = savedJob.map((id) => jobs.find((job) => job.id === id));
+    const [filter, setFilter] = useState(null);
 
-  const filteredJobs = filter
-    ? jobCart.filter((job) => job.jobType === filter) : jobCart;
+    const jobCart = savedJob ? savedJob.map((id) => jobs.find((job) => job.id === id)) : [];
 
-  return (
-    <div>
-      <h2 className="text-center mt-12 font-semibold text-2xl">Applied Jobs</h2>
+    const filteredJobs = filter ? jobCart.filter((job) => job.jobType === filter) : jobCart;
 
-      <div className="flex justify-end space-x-4 mt-4 mr-12">
-        <button
-          className={`px-4 py-2 rounded-md border border-purple-500 ${
-            filter === 'Remote' ? 'bg-violet-500 text-white' : 'bg-white text-violet-500'
-          }`}
-          onClick={() => setFilter('Remote')}
-        >
-          Remote
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md border border-purple-500 ${
-            filter === 'Onsite' ? 'bg-violet-500 text-white' : 'bg-white text-violet-500'
-          }`}
-          onClick={() => setFilter('Onsite')}
-        >
-          Onsite
-        </button>
-      </div>
+    return (
+        <div>
+            <div className="bg-gradient-to-r from-indigo-400 via-purple-600 to-purple-400 py-3 rounded">
+                <h2 className="text-center mt-12 font-semibold text-4xl pb-12 text-white">Applied Jobs</h2>
+            </div>
 
-      {filteredJobs.map((job) => (
-        <ApplyJob key={job.id} job={job} />
-      ))}
-    </div>
-  );
+            <div className="flex justify-end space-x-4 mt-4 mr-12">
+                <button
+                    className={`px-4 py-2 rounded-md border border-purple-500 ${filter === 'Remote' ? 'bg-violet-500 text-white' : 'bg-white text-violet-500'
+                        }`}
+                    onClick={() => setFilter('Remote')}
+                >
+                    Remote
+                </button>
+                <button
+                    className={`px-4 py-2 rounded-md border border-purple-500 ${filter === 'Onsite' ? 'bg-violet-500 text-white' : 'bg-white text-violet-500'
+                        }`}
+                    onClick={() => setFilter('Onsite')}
+                >
+                    Onsite
+                </button>
+            </div>
+
+            {filteredJobs.map((job) => (
+                <ApplyJob key={job.id} job={job} />
+            ))}
+        </div>
+    );
 };
 
 export default AppliedJobs;
